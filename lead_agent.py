fa9b064568577f78abb6a96a7af97bde4a018b73
gxt <gauravkthakur2002@gmail.com>
2026-04-23 10:22:53 +0530

    refactor: clean up unused imports in lead agent file

--- lead_agent.py
+++ lead_agent.py
@@ -1,18 +1,11 @@
 import asyncio
-import os
 from dotenv import load_dotenv
-import json
-import sqlite3
-from typing import List, TypedDict, Annotated, Optional
+from typing import TypedDict, Annotated
 from langchain_core.messages import HumanMessage, AnyMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph.message import add_messages
-from langchain_core.prompts import PromptTemplate
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
-from langgraph.types import interrupt # For HITL
-from langgraph.types import Command
-from langchain_core.messages import ToolMessage
 from tools import mock_lead_capture
 
 load_dotenv()
